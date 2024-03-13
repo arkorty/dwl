39644a74aa806fd32d06ac40fe244ca96232b41b
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-03-14 00:10:26 +0530

    Add keyboard layout rule to swap Esc with Capslock

--- config.def.h
+++ config.def.h
@@ -53,7 +53,7 @@ static const struct xkb_rule_names xkb_rules = {
 	/* example:
 	.options = "ctrl:nocaps",
 	*/
-	.options = NULL,
+	.options = "caps:swapescape",
 };
 
 static const int repeat_rate = 25;
